84b53c9fe660564c7385283752512fd8b4d5542e
bruhGrassi <brunagm88@gmail.com>
2024-05-28 18:27:02 -0300

    turn js config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+// import $ from "jquery";
+// import * as $ from 'jquery';
+// import 'turn.js'
+import 'js1/turn.js'
+// declare const teste:any;
+declare const $:any;
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'flipbook';
+  callfun(){
+    // teste();
+    (function($){})
+  }
+
+  goRight(){
+    document.getElementById('wrapper').style.marginLeft = '10%';
+  }
+
+  ngOnInit() {
+
+    // $(window).click(function(){
+    //   alert('clicou');
+    //    });
+
+    $( window).ready(function() {
+      $("#magazine").turn({
+        autoCenter: true,
+        display: 'double',
+        acceleration: true,
+        gradients: !$.isTouch,
+        elevation:50,      
+      });
+
+      $(window).bind('keydown', function(e){
+        
+        if (e.keyCode==37)
+          $('#magazine').turn('previous');
+        else if (e.keyCode==39)
+          $('#magazine').turn('next');       
+      });    
+    })
+  } 
+  
+
+
+
 }
---
+++ src/assets/js/teste.js
@@ -0,0 +1,3 @@
+function teste(){
+    alert('deu certo!')
+}
